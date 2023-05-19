438d72a475cc094cb9c03aa387bf0d801af34513
blthai <39754216+blthai@users.noreply.github.com>
2023-05-18 17:23:04 -0700

    add functions to check for a winner for matching rows and columns

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const boardSlots = document.querySelectorAll(".board-slot");
 
-const createPlayer = (name, playerNumber) => {
-  return { name, playerNumber };
-};
+const createPlayer = (name, playerNumber) => ({ name, playerNumber });
 
 const playerOne = createPlayer('Benji', 1);
 const playerTwo = createPlayer('Maylinh', 2);
@@ -11,6 +9,8 @@ const gameBoard = (() => {
   const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   let currentTurn = 1;
   let slotIndex = 0;
+  let winnerExists = false;
+  let tieExists = false;
   
   const displayBoard = () => {
     let index = 0;
@@ -27,30 +27,74 @@ const gameBoard = (() => {
     index = 0;
   };
 
+  const checkColumn = (start, end, playerNumber) => {
+    let winningColumn = true;
+    board.slice(start, end).forEach((index) => {
+      if(index !== playerNumber){
+        
+        winningColumn = false;
+      }
+    })    
+    return winningColumn;
+  }
+
+  const checkRow = (chosenRow, playerNumber) => {
+    let winningRow = true;
+    chosenRow.forEach((tile) => {if(board[tile]!==playerNumber){winningRow=false;}})
+    return winningRow;
+  }
+
+
+
+  const checkWinner = (playerNumber, chosenTile) => {
+    const winningArray = [playerNumber, playerNumber, playerNumber];
+    if([0, 1, 2].includes(chosenTile)){
+      if(checkColumn(0, 3, playerNumber) || checkRow([chosenTile, chosenTile + 3, chosenTile + 6, playerNumber])){
+        winnerExists = true;
+      }
+    }
+    if([3, 4, 5].includes(chosenTile)){
+      if(checkColumn(3, 6, playerNumber) || checkRow([chosenTile, chosenTile - 3, chosenTile + 3, playerNumber])){
+        winnerExists = true;
+      }
+    }
+    if([6, 7, 8].includes(chosenTile)){
+      if(checkColumn(6, 9, playerNumber) || checkRow([chosenTile, chosenTile - 3, chosenTile - 3, playerNumber])){
+        winnerExists = true;
+      }
+    }
+    
+    else if(!board.includes(0)){
+      tieExists = true;
+    }
+  };
+
   const markTile = (event) => {
-    const chosenBoardTile = event.target.dataset.index;
+    if(winnerExists){
+      return;
+    }
+    const chosenBoardTile = Number(event.target.dataset.index);
     if(board[chosenBoardTile]!==0){
       return;
     }
     if(currentTurn % 2 !== 0){
       board[chosenBoardTile] = playerOne.playerNumber;
+      checkWinner(playerOne.playerNumber, chosenBoardTile);
+      
     }else{
       board[chosenBoardTile] = playerTwo.playerNumber;
+      checkWinner(playerTwo.playerNumber, chosenBoardTile);
     }
     currentTurn+=1;
     gameBoard.displayBoard();
   };
-
   boardSlots.forEach((slot) => {
     slot.dataset.index = slotIndex;
     slotIndex+=1;
     slot.addEventListener('click', markTile);
   })
 
-
-  const checkWinner = () => {
-    
-  };
+  
 
   const playRound = () => {
 
@@ -58,21 +102,20 @@ const gameBoard = (() => {
   };
   
 
-  return { board, playRound};
+  return { board, playRound, displayBoard, winnerExists};
 })();
 
 
 
 
 const gameFlow = (() => {
-  let winnerExists = false;
-
   const startGame = () => {
-    while(winnerExists === false){
+    while(gameBoard.winnerExists === false){
       gameBoard.playRound();
     }
+
   };
-  
+  return {};
 })();
 
 gameBoard.displayBoard();

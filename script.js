857393e4750d6ea6042cd604f6c574a940c15666
blthai <39754216+blthai@users.noreply.github.com>
2023-05-19 19:47:00 -0700

    allow players to enter their names, start the game, and reset the game

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const gameBoard = (() => {
   const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   let winnerExists = false;
   let tieExists = false;
+  let gameStart = false;
 
   const displayBoard = () => {
     let index = 0;
@@ -18,6 +19,12 @@ const gameBoard = (() => {
       index += 1;
     });
     index = 0;
+  };
+
+  const resetBoard = () => {
+    for (let index = 0; index < board.length; index += 1) {
+      board[index] = 0;
+    }
   };
 
   const checkColumn = (start, end, playerNumber) => {
@@ -50,7 +57,7 @@ const gameBoard = (() => {
     return winningDiagonal;
   };
 
-  const checkWinner = (playerNumber, chosenTile) => {  
+  const checkWinner = (playerNumber, chosenTile) => {
     const winningArray = [playerNumber, playerNumber, playerNumber];
     if ([0, 1, 2].includes(chosenTile)) {
       if (
@@ -88,7 +95,18 @@ const gameBoard = (() => {
     }
   };
   const getWinnerExists = () => winnerExists;
-  return { board, displayBoard, checkWinner, getWinnerExists};
+  const falsifyWinnerExists = () => {
+    winnerExists = false;
+  };
+  return {
+    board,
+    displayBoard,
+    checkWinner,
+    getWinnerExists,
+    gameStart,
+    resetBoard,
+    falsifyWinnerExists,
+  };
 })();
 
 const Player = (playerName, playerNumber) => {
@@ -98,36 +116,98 @@ const Player = (playerName, playerNumber) => {
     }
     gameBoard.board[chosenBoardTile] = playerNumber;
   };
-  return { playerName, playerNumber, markTile}
-  };
- 
+  return { playerName, playerNumber, markTile };
+};
+
 const gameFlow = (() => {
   let currentTurn = 1;
-  const playerOne = Player("Benji", 1);
-  const playerTwo = Player("Maylinh", 2);
+  let playerOne;
+  let playerTwo;
+
+  const createPlayers = (playerOneName, playerTwoName) => {
+    playerOne = Player(playerOneName, 1);
+    playerTwo = Player(playerTwoName, 2);
+  };
+
+  const resetTurns = () => {
+    currentTurn = 1;
+  };
   const playTurn = (event) => {
-    if (gameBoard.getWinnerExists()) {
+    if (gameBoard.getWinnerExists() || gameBoard.gameStart === false) {
       return;
     }
+    console.log(currentTurn);
     const chosenBoardTile = Number(event.target.dataset.index);
     if (currentTurn % 2 !== 0) {
       playerOne.markTile(chosenBoardTile);
-      gameBoard.checkWinner(playerOne.playerNumber, chosenBoardTile)
+      gameBoard.checkWinner(playerOne.playerNumber, chosenBoardTile);
+    } else {
+      playerTwo.markTile(chosenBoardTile);
+      gameBoard.checkWinner(playerTwo.playerNumber, chosenBoardTile);
     }
-    else{playerTwo.markTile(chosenBoardTile);
-      gameBoard.checkWinner(playerTwo.playerNumber, chosenBoardTile)}
     currentTurn += 1;
     gameBoard.displayBoard();
   };
-  return {playTurn};
+  return { playTurn, playerOne, playerTwo, resetTurns, createPlayers };
+})();
+
+const displayController = (() => {
+  const newGameForm = document.querySelector(".new-game-form");
+
+  const initializeDisplay = () => {
+    const playerOneInput = document.createElement("input");
+    playerOneInput.id = "playerone";
+    playerOneInput.required = "true";
+    const playerOneLabel = document.createElement("label");
+    playerOneLabel.htmlFor = "playerone";
+    playerOneLabel.textContent = "Player One: ";
+    const playerTwoInput = document.createElement("input");
+    playerTwoInput.id = "playertwo";
+    playerTwoInput.required = "true";
+    const playerTwoLabel = document.createElement("label");
+    playerTwoLabel.htmlFor = "playertwo";
+    playerTwoLabel.textContent = "Player Two: ";
+    const submitPlayersButton = document.createElement("button");
+    submitPlayersButton.type = "submit";
+    submitPlayersButton.textContent = "Start Game!";
+    newGameForm.appendChild(playerOneLabel);
+    newGameForm.appendChild(playerOneInput);
+    newGameForm.appendChild(playerTwoLabel);
+    newGameForm.appendChild(playerTwoInput);
+    newGameForm.appendChild(submitPlayersButton);
+    newGameForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      gameFlow.createPlayers(playerOneInput.value, playerTwoInput.value);
+      gameBoard.gameStart = true;
+      while (newGameForm.firstChild) {
+        newGameForm.removeChild(newGameForm.firstChild);
+      }
+      const resetButton = document.createElement("button");
+      resetButton.textContent = "Reset Game";
+      resetButton.addEventListener("click", resetGame);
+      newGameForm.appendChild(resetButton);
+    });
+  };
+
+  const resetGame = () => {
+    newGameForm.removeChild(newGameForm.firstChild);
+    gameBoard.resetBoard();
+    gameBoard.displayBoard();
+    gameBoard.gameStart = false;
+    gameBoard.falsifyWinnerExists();
+    gameFlow.resetTurns();
+    initializeDisplay();
+  };
+
+  return { initializeDisplay };
 })();
 
 let slotIndex = 0;
 boardSlots.forEach((slot) => {
   slot.dataset.index = slotIndex;
   slotIndex += 1;
-  //slot.addEventListener("click", gameBoard.markTile);
   slot.addEventListener("click", gameFlow.playTurn);
 });
 
+displayController.initializeDisplay();
 gameBoard.displayBoard();
